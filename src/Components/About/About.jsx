90d2e9884f91d60b3ecbb09fd2c019732ab86893
Puspalal <puspalalnewar79@gmail.com>
2024-08-10 21:47:53 +0530

    Working on about section

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import './About.css'
+import about_img from '../../Assets/about-img.jpg'
 
 const About = () => {
   return (
     <div className='about'>
-      
+      <div className="about-left">
+        <img src={about_img} alt="" />
+        <i class="fa-solid fa-play"></i>
+      </div>
+      <div className="about-right">
+        <h3>ABOUT UNIVERSITY</h3>
+        <h2>Nurturning Tomorrow's Leader's Today</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A autem doloribus repellendus debitis beatae commodi ad. Eaque facere animi molestias architecto ea quas magnam itaque aut placeat similique. Enim sapiente dolore magnam consequatur, ex tempore provident?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum adipisci atque quaerat, sequi iusto tempora velit libero sint aperiam sunt modi facilis deleniti nisi veritatis.</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum, veniam. Necessitatibus ea similique illo numquam dolorum est consectetur vero vel!</p>
+      </div>
     </div>
   )
 }
