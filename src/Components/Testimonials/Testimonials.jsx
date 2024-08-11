87608eb53beb53fa8e637e5db555d9767174039c
Puspalal <puspalalnewar79@gmail.com>
2024-08-11 08:50:57 +0530

    Working on testimonials section

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -4,7 +4,16 @@ import './Testimonials.css'
 const Testimonials = () => {
   return (
     <div className='testimonials'>
-      
+      <div className="design">
+        <i class="fa-solid fa-arrow-right"></i>
+      </div>
+      <div className="design design-back">
+        <i class="fa-solid fa-arrow-left "></i>
+      </div>
+
+      <div className="slider">
+
+      </div>
     </div>
   )
 }
